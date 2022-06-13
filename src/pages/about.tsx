db5afccc895338ce7e74c18c68faafa7b8ef6087
igor s fernandes <igorsife80@gmail.com>
2022-06-13 18:16:43 -0300

    adiciona a estrutura inicial da pagina about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { DivAboutBody, MainAboutDiv } from '../styles/Pages/aboutStyles/aboutStyles';
 
 const About = () => {
   return (
-    <div>
+    <DivAboutBody>
       <NavBar />
-      about
-    </div>
+      <MainAboutDiv>
+        <h1>Sobre mim </h1>
+        <p>
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+          sobre mim sobre mim sobre mim sobre mim <br />
+          sobre mim sobre mim sobre mim sobre mim
+        </p>
+      </MainAboutDiv>
+    </DivAboutBody>
   )
 }
 
---
+++ src/styles/Pages/aboutStyles/aboutStyles.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const DivAboutBody = styled.div`
+  min-height: 100vh;
+`;
+
+export const MainAboutDiv = styled.div`
+  border: 1px solid white;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+
+  margin-top: 5%;
+
+  h1 {
+     font-size: 100px;
+    font-weight: bolder;
+  }
+
+  p {
+    font-size: x-large;
+    font-weight: bolder;
+  }
+`;
