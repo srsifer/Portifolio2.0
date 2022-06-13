ad7207cdb2827e3986fbcd0a54d9067f21ee1288
igor s fernandes <igorsife80@gmail.com>
2022-06-13 16:12:02 -0300

    feat:cria a pagina de contatos com o gif

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,12 +1,70 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import emailIcon from '../assets/email.png'
+import instagramIcon from '../assets/instagram.png'
+import linkedInIcon from '../assets/linkedin.png'
+import whatsAppIcon from '../assets/whatsapp.png'
+import { DiGithubBadge } from "react-icons/di";
+import Image from 'next/image'
+import { ContactDivCards, ContactList, ContactsDivBody, DivIconsText } from '../styles/Pages/contactsStyles/contactStyles'
+
+const { src: email }: any = emailIcon;
+const { src: instagram }: any = instagramIcon;
+const { src: linkedIn }: any = linkedInIcon;
+const { src: whatsApp }: any = whatsAppIcon;
 
 const Contacts = () => {
   return (
-    <div>
+    <ContactsDivBody>
       <NavBar />
-      contacts
-    </div>
+      <ContactList>
+        <ContactDivCards>
+          <div>
+            <h2>Email</h2>
+          </div>
+          <DivIconsText>
+            <Image src={email} alt="IMAGEM DE EMAIL" width="100" height="100" />
+            <p>pequena descrição</p>
+          </DivIconsText>
+        </ContactDivCards>
+        <ContactDivCards>
+          <div>
+            <h2>linkedIn</h2>
+          </div>
+          <DivIconsText>
+            <Image src={linkedIn} alt="icone do linked-IN" width="100" height="100" />
+            <p>pequena descrição</p>
+          </DivIconsText>
+        </ContactDivCards>
+        <ContactDivCards>
+          <div>
+            <h2>gitHub</h2>
+          </div>
+          <DivIconsText>
+            <DiGithubBadge />
+            <p>pequena descrição </p>
+          </DivIconsText>
+        </ContactDivCards>
+        <ContactDivCards>
+          <div>
+            <h2>Instagra</h2>
+          </div>
+          <DivIconsText>
+            <Image src={instagram} alt="icone do instagram" width="100" height="100" />
+            <p>pequena descrição</p>
+          </DivIconsText>
+        </ContactDivCards>
+        <ContactDivCards>
+          <div>
+            <h2>whatsApp</h2>
+          </div>
+          <DivIconsText>
+            <Image src={whatsApp} alt="icone do whats app" width="100" height="100" />
+            <p>pequena descrição</p>
+          </DivIconsText>
+        </ContactDivCards>
+      </ContactList>
+    </ContactsDivBody>
   )
 }
 
---
+++ src/styles/Pages/contactsStyles/contactStyles.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+import gif from '../../../assets/giftec.gif'
+
+const { src }: any = gif;
+
+export const ContactsDivBody = styled.div`
+  background-image: url(${src});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  min-width: auto;
+  height: 100vh;
+
+ h2 {
+   color: ${({ theme }) => theme.colors.gray}
+ }
+
+ img {
+   width: 100px;
+   height: 100px;
+   border-radius: 10px;
+ }
+ svg {
+  width: 100px;
+   height: 100px;
+   border-radius: 10px;
+
+ }
+`;
+export const ContactDivCards = styled.div`
+  background-color: #1f7fb676;
+  border-radius: 15px;
+
+  display:flex ;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 35%;
+  margin: 20px;
+  padding: 15px;
+    :hover {
+    background-color: #1d5cb9e1;
+    transform: scale(1.1);
+    box-shadow: 0 10px 10px 0 #012235 ;
+    border-radius: 10px;
+    }
+
+`;
+
+export const ContactList = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+
+`;
+
+export const DivIconsText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  font-size: x-large;
+  font-weight: bold;
+`
